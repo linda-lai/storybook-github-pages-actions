7fda13a9aad34b0d006b40f54be5a20bbe9897fd
Linda Lai <lindalai.615@gmail.com>
2022-03-19 02:09:40 +1100

    test: Trigger changes to test Storybook cleanup job

--- src/stories/Header.jsx
+++ src/stories/Header.jsx
@@ -35,7 +35,7 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
         {user ? (
           <>
             <span className="welcome">
-              Welcome, <b>{user.name}</b>! ❤️
+              Welcome, <b>{user.name}</b>! 👋
             </span>
             <Button size="small" onClick={onLogout} label="Log out" />
           </>
